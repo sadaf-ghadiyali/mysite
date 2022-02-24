9ba006ed5cdfbeb5f2798d8009aa3773d21512f4
sadafghadiyali <sadaf.ghadiyali@ugamsolutions.com>
2022-02-24 23:06:20 +0530

    Jackson Content Exporter

--- core/src/main/java/com/mysite/core/models/Impl/AuthorImpl.java
+++ core/src/main/java/com/mysite/core/models/Impl/AuthorImpl.java
@@ -1,13 +1,13 @@
 package com.mysite.core.models.Impl;
 
 import com.day.cq.wcm.api.Page;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import com.mysite.core.models.Author;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.models.annotations.Default;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.*;
 import org.apache.sling.models.annotations.injectorspecific.*;
 
 import org.apache.sling.api.resource.Resource;
@@ -16,10 +16,20 @@ import javax.inject.Inject;
 
 @Model(adaptables = SlingHttpServletRequest.class,
         adapters = Author.class,
+        resourceType = AuthorImpl.RESOURCE_TYPE,
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name="jackson",extensions = "json",selector="site",
+            options = {
+                    @ExporterOption(name = "MapperFeature.SORT_PROPERTIES_ALPHABETICALLY", value="true"),
+                @ExporterOption(name= "SerializationFeature.WRAP_ROOT_VALUE",value="true")
+
+            }
+)
+
+@JsonRootName("author-details")
 public class AuthorImpl implements Author{
 
-
+    static final String RESOURCE_TYPE="mysite/components/author";
 
     @SlingObject
     ResourceResolver resourceResolver;
@@ -42,6 +52,11 @@ public class AuthorImpl implements Author{
     @Default(values="SITE")
     String lname;
 
+    @JsonProperty(value = "sample")
+    public String propertyMethod(){
+        return "JSON Property Example";
+    }
+
     @Override
     public String getFirstName(){
         return fname;
@@ -51,10 +66,13 @@ public class AuthorImpl implements Author{
         return lname;
     }
 
-    public String getCurrentPage() {
-        return currentPage.getName();
-    }
 
+    @Override
+    @JsonProperty(value="Page Title")
+    public String getCurrentPage() {
+        return currentPage.getTitle();
+    }
+    @JsonIgnore
     public String getMyAttribute() {
         return myAttribute;
     }
